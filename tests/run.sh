ac2a0852a6472d0b5d677e49c557011ec97b4586
Michael Adam <obnox@redhat.com>
2017-03-24 21:07:08 +0100

    SQ: fix [[ in runner

--- tests/run.sh
+++ tests/run.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 for testdir in ${SCRIPT_DIR}/*; do
-	if [ ! -d ${testdir} ]; then
+	if [[ ! -d ${testdir} ]]; then
 		continue
 	fi
 
-	if [ ! -x ${testdir}/run.sh ]; then
+	if [[ ! -x ${testdir}/run.sh ]]; then
 		continue
 	fi
 
@@ -16,7 +16,7 @@ for testdir in ${SCRIPT_DIR}/*; do
 	rc=$?
 	popd
 
-	if [ ${rc} -ne 0 ]; then
+	if [[ ${rc} -ne 0 ]]; then
 		exit 1
 	fi
 done
