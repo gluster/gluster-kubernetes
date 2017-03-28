d873927682b7cda0b51d9206eba064e630ff2ca6
Michael Adam <obnox@redhat.com>
2017-03-29 01:06:28 +0200

    vagrant:demo: clean up variables a bit
    Only set those variables really needed in the scripts.
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- vagrant/demo/demo-inside-wrapper.sh
+++ vagrant/demo/demo-inside-wrapper.sh
@@ -2,8 +2,6 @@
 
 DEMO_DIR="$(cd $(dirname $0) ; pwd)"
 VAGRANT_DIR="${DEMO_DIR}/.."
-DEPLOY_DIR="${VAGRANT_DIR}/../deploy"
-
 SSH_CONFIG=${DEMO_DIR}/ssh-config
 
 cd ${VAGRANT_DIR}
--- vagrant/demo/demo-prepare.sh
+++ vagrant/demo/demo-prepare.sh
@@ -3,7 +3,6 @@
 DEMO_DIR="$(cd $(dirname $0) ; pwd)"
 VAGRANT_DIR="${DEMO_DIR}/.."
 DEPLOY_DIR="${VAGRANT_DIR}/../deploy"
-
 SSH_CONFIG=${DEMO_DIR}/ssh-config
 
 cd ${VAGRANT_DIR}
@@ -12,7 +11,7 @@ vagrant up
 
 vagrant ssh-config > ${SSH_CONFIG}
 
-scp -r -F ssh-config ${DEPLOY_DIR} master:
+scp -r -F "${SSH_CONFIG}" "${DEPLOY_DIR}" master:
 
 for NODE in node0 node1 node2 ; do
 	ssh -t -F ${SSH_CONFIG} $NODE "sudo docker pull gcr.io/google_containers/nginx-slim:0.8"
