9641ef4b662cc9833ffdd03275475f96bd595480
Michael Adam <obnox@redhat.com>
2017-03-30 19:15:20 +0200

    test: add first functional tests
    These functional tests basically run the steps of the
    quickstart guide and the dynamic provisioning example.
    
    ./test.sh will bring up vagrant, run all tests and will
    destroy the vagrant environment again.
    
    Tests can also be run individually against an already
    running vagrant environment:
    
    ./test-gk-deploy.sh
    ./test-dynamic-provisioning.sh
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

---
+++ tests/functional/test-inside-gk-deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# test gk-deploy
+
+cd ~/deploy
+
+num_gluster_pods_before=$(kubectl get pods | grep -s "glusterfs-" | wc -l)
+num_heketi_pods_before=$(kubectl get pods | grep -s "heketi-" | wc -l)
+
+./gk-deploy -y -g ./topology.json
+
+if [[ $? -ne 0 ]]; then
+	echo "ERROR: gk-deploy failed"
+	exit 1
+fi
+
+# wait briefly for pods to settle down...
+sleep 2
+
+num_gluster_pods_after=$(kubectl get pods | grep -s "glusterfs-" | wc -l)
+num_heketi_pods_after=$(kubectl get pods | grep -s "heketi-" | grep -vs "heketi-deploy" | wc -l)
+
+if (( num_heketi_pods_after - num_heketi_pods_before != 3 )); then
+	echo "ERROR: unexpected number of heketi pods: " \
+		"${num_heketi_pods_after} - " \
+		"expected $(( num_heketi_pods_before + 1 ))"
+	exit 1
+fi
+
+if (( num_gluster_pods_after - num_gluster_pods_before != 3 )); then
+	echo "ERROR: unexpected number of gluster pods: " \
+		"${num_gluster_pods_after} - " \
+		"expected $(( num_gluster_pods_before + 3 ))"
+	exit 1
+fi
+
+echo "PASS"
+exit 0
