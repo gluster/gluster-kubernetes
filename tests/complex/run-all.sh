d6827d3a11655ec7b001036c47b65cce6cdb0de1
Jose A. Rivera <jarrpa@redhat.com>
2017-08-17 08:26:20 -0500

    tests/complex: Add tests for deploying block provisioner pod
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- tests/complex/run-all.sh
+++ tests/complex/run-all.sh
@@ -12,8 +12,14 @@ run -e "${TEST_DIR}/run-basic.sh"
 
 rollback_vagrant
 
+run "${TEST_DIR}/run-block.sh"
+
+rollback_vagrant
+
 run "${TEST_DIR}/run-object.sh"
 
-run "${TEST_DIR}/test-object-store.sh"
+rollback_vagrant
+
+run "${TEST_DIR}/run-full.sh"
 
 run "${TEST_DIR}/test-teardown.sh"
