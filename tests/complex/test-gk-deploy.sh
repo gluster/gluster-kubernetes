15d50a9c4a6229860aaab9c2cb313cfd5de37f3e
Jose A. Rivera <jarrpa@redhat.com>
2017-08-17 07:43:08 -0500

    tests/complex: Overhaul test framework
    This is a collection of a few inter-related changes I didn't feel like
    splitting apart. :) The intent was to visually enhance the testing framework
    while also reducing the amount of logic and coding that needs to go into
    writing new tests:
    
     * Implement a single run() function for local and remote tests. This
       facilitates test progress output.
     * Implement a subtest framework to track which test in a file we are one.
       Allow specifying whether to exit on failure or not with a flag.
     * Implement a "run" framework. A "run" is a series of tests defined by
       whatever test script you run.
     * Move a lot of common behaviors into lib.
     * Add end-of-test functions as triggers on system signals.
     * Enhance test output with pretty colors! :)
    
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- tests/complex/test-gk-deploy.sh
+++ tests/complex/test-gk-deploy.sh
@@ -4,10 +4,9 @@ TEST_DIR="$(realpath $(dirname $0))"
 
 source "${TEST_DIR}/lib.sh"
 
-ssh_config || fail "ERROR to creating ssh-config"
+copy_deploy
 
-copy_deploy || fail "ERROR copying deployment files"
+run -r master -e "${TEST_DIR}/test-inside-gk-deploy.sh" "Test basic deployment"
 
-run_on_node "${TEST_DIR}/test-inside-gk-deploy.sh" master || fail
+run -r master "${TEST_DIR}/test-inside-gk-deploy.sh" "Test basic deployment idempotence"
 
-pass
