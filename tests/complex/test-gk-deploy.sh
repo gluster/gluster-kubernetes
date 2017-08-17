80b5febfaf9428686a88a757df5e14caf2cf1961
Jose A. Rivera <jarrpa@redhat.com>
2017-08-17 08:26:20 -0500

    tests/complex: Add tests for deploying S3 pods
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- tests/complex/test-gk-deploy.sh
+++ tests/complex/test-gk-deploy.sh
@@ -10,3 +10,10 @@ run -r master -e "${TEST_DIR}/test-inside-gk-deploy.sh" "Test basic deployment"
 
 run -r master "${TEST_DIR}/test-inside-gk-deploy.sh" "Test basic deployment idempotence"
 
+rollback_vagrant
+
+copy_deploy
+
+run -r master -e "${TEST_DIR}/test-inside-gk-deploy.sh obj" "Test full deployment"
+
+run -r master "${TEST_DIR}/test-inside-gk-deploy.sh obj" "Test full deployment idempotence"
