a042d2249cf536e755a167b33d98a1978562aaeb
Jose A. Rivera <jarrpa@redhat.com>
2017-08-22 11:49:03 -0500

    tests/simple/gk-deploy: Improve cli stubs
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

---
+++ tests/simple/gk-deploy/stubs/cli.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ "${@}" == *"get "* ]]; then
+	while [[ "${1}" != "get" ]]; do
+		shift
+	done
+	shift
+	restype="${1}"
+	shift
+	while [[ "${1}" != --selector* ]] && [[ "${1}" == --* ]]; do
+		shift
+	done
+	select="${1}"
+	if [[ "${restype}" == namespace* ]] && [[ "${select}" == "invalid" ]]; then
+		echo "Error"
+	fi
+elif [[ "${@}" == *" config get-contexts" ]]; then
+	if [[ "${0}" == *oc* ]]; then
+		echo "* two three four storage"
+	fi
+fi
+
+exit 0
